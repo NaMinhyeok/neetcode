c6c36f780f1e5ad5ed9c010d277c594e7fb1184c
NaMinhyeok <nmh9097@gmail.com>
2026-02-11 00:22:22 +0900

    121. Best Time to Buy And Sell Stock

--- problems/sliding-window/0121-best-time-to-buy-and-sell-stock/solution.py
+++ problems/sliding-window/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -3,5 +3,12 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 class Solution:
+    # 시간복잡도 O(N^2)
+    # 공간복잡도 O(N)
     def maxProfit(self, prices: List[int]) -> int:
-        
+        result = 0
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                result = max(prices[j] - prices[i], result)
+
+        return result
