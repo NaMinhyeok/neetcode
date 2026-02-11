11d6077f358b433719fa30298f21a0a7fe5e7e5a
NaMinhyeok <nmh9097@gmail.com>
2026-02-11 22:20:58 +0900

    review

--- problems/sliding-window/0121-best-time-to-buy-and-sell-stock/solution.py
+++ problems/sliding-window/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -2,13 +2,18 @@
 # 121. Best Time to Buy And Sell Stock (Easy)
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
-class Solution:
-    # 시간복잡도 O(N^2)
-    # 공간복잡도 O(N)
-    def maxProfit(self, prices: List[int]) -> int:
-        result = 0
-        for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
-                result = max(prices[j] - prices[i], result)
 
-        return result
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(profit, maxP)
+            else:
+                l = r
+            r += 1
+
+        return maxP
--- problems/two-pointers/0125-valid-palindrome/solution.py
+++ problems/two-pointers/0125-valid-palindrome/solution.py
@@ -2,22 +2,12 @@
 # 125. Valid Palindrome (Easy)
 # https://leetcode.com/problems/valid-palindrome/
 
+
 class Solution:
-    # 시간복잡도: O(N)
-    # 공간복잡도: O(N)
     def isPalindrome(self, s: str) -> bool:
-        result = re.sub(r'[^a-zA-Z0-9]','',s).strip().replace(" ", "").lower()
-        candidates = list(result)
-        center = len(result)//2
-        if len(result) % 2 == 0:
-            forward = candidates[:center]
-            backward = candidates[center:]
-            print(f"forward: {forward}")
-            print(f"backward: {backward}")
-            return forward == backward[::-1]
-        else:
-            forward = candidates[:center]
-            backward = candidates[center+1:]
-            print(f"forward: {forward}")
-            print(f"backward: {backward}")
-            return forward == backward[::-1]
+        newStr = ""
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+
+        return newStr == newStr[::-1]
