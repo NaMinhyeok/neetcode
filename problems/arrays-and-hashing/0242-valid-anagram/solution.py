e2a4d4afe7f177024648ae2bb6793ef85610dc54
NaMinhyeok <nmh9097@gmail.com>
2026-02-11 21:47:42 +0900

    valid-anagram

--- problems/arrays-and-hashing/0242-valid-anagram/solution.py
+++ problems/arrays-and-hashing/0242-valid-anagram/solution.py
@@ -2,6 +2,21 @@
 # 242. Valid Anagram (Easy)
 # https://leetcode.com/problems/valid-anagram/
 
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
+        sDict = dict()
+        tDict = dict()
+        for val in s:
+            if val in sDict:
+                sDict[val] += 1
+            else:
+                sDict[val] = 1
+
+        for val in t:
+            if val in tDict:
+                tDict[val] += 1
+            else:
+                tDict[val] = 1
+
+        return sDict == tDict
