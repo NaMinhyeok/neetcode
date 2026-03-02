d606eedf35190b4b283c9c0451d4a795455f207c
NaMinhyeok <nmh9097@gmail.com>
2026-03-02 17:11:38 +0900

    group ananagram

--- problems/arrays-and-hashing/0049-group-anagrams/solution.py
+++ problems/arrays-and-hashing/0049-group-anagrams/solution.py
@@ -2,6 +2,16 @@
 # 49. Group Anagrams (Medium)
 # https://leetcode.com/problems/group-anagrams/
 
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
+        maps = defaultdict(list)
+        for s in strs:
+            count = [0] * 26  # a to z
+
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            maps[tuple(count)].append(s)
+
+        return list(maps.values())
